d3f818e8645d5d1c261858a7f14b7601d605d100
Sam Ouyang <saouyang@ucsd.edu>
2025-02-20 11:09:27 -0800

    ahhh

--- map.js
+++ map.js
@@ -1,12 +1,61 @@
 // Set your Mapbox access token here
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2Ftb3V5YW5nIiwiYSI6ImNtN2IzcG13dDAycGgya29kNnNkdTh2cmoifQ.CqpS-hfcWIEa1VjXldUEkA';
 
-// Initialize the map
-const map = new mapboxgl.Map({
-  container: 'map', // ID of the div where the map will render
-  style: 'mapbox://styles/mapbox/streets-v12', // Map style
-  center: [-71.09415, 42.36027], // [longitude, latitude]
-  zoom: 12, // Initial zoom level
-  minZoom: 5, // Minimum allowed zoom
-  maxZoom: 18 // Maximum allowed zoom
-});
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize the map
+    const map = new mapboxgl.Map({
+      container: 'map', // ID of the div where the map will render
+      style: 'mapbox://styles/mapbox/streets-v12', // Map style
+      center: [-71.09415, 42.36027], // [longitude, latitude]
+      zoom: 12, // Initial zoom level
+      minZoom: 5, // Minimum allowed zoom
+      maxZoom: 18 // Maximum allowed zoom
+    });
+
+    map.on('load', () => { 
+        map.addSource('boston_route', {
+            type: 'geojson',
+            data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...'
+            });
+    
+        map.addLayer({
+            id: 'boston-bike-lanes',
+            type: 'line',
+            source: 'boston_route',
+            paint: {
+                'line-color': '#37FF33',  // A bright green using hex code
+                'line-width': 4,          // Thicker lines
+                'line-opacity': 0.6       // Slightly less transparent
+              }
+            });
+
+        map.addSource('cambridge_route', {
+            type: 'geojson',
+            data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson?...'
+            });
+        
+        map.addLayer({
+            id: 'cambridge-bike-lanes',
+            type: 'line',
+            source: 'cambridge_route',
+            paint: {
+                'line-color': '#37FF33',  // A bright green using hex code
+                'line-width': 4,          // Thicker lines
+                'line-opacity': 0.6       // Slightly less transparent
+                }
+            });
+
+
+
+
+
+
+
+        });
+
+
+  });
+
+
+
+  
